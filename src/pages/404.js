d6b0e5091f05adf436711e62d8f8a9e952cab497
Shugaku <shugakuiwafuchi@gmail.com>
2020-11-01 18:15:45 +0900

    １回目の変更

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = ({ data }) => (
   <div className="container">
     <div className="row">
       <div className="col">
-        <h4>找不到你的网页，本站所有页面为：</h4>
+        <h4>ページが見つかりません。</h4>
         {data.allSitePage.edges.map(page => (
           <Link to={page.node.path} href={page.node.path} key={page.node.path}>
             <li>{page.node.path}</li>
